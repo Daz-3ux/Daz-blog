c4e2bbd1fc488923d06b79114202738f57c24fe9
Daz-3ux <1293821619@qq.com>
2022-09-30 22:53:29 +0800

    config

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,8 +1,8 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: 'one',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  // { text: 'one',link: '/one/',},
+  // { text: 'two',link: '/two/',},
+  // { text: 'three', link: '/three/' },
   { text: '留言板', link: '/message-board/' },
 ]
